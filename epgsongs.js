e0439e167a110be8bbb26223dfdbc7e5648efa54
barney-heatwave <djdise@outlook.com>
2020-05-10 23:17:52 +0100

    Fix incorrect track names

--- epgsongs.js
+++ epgsongs.js
@@ -1,6 +1,6 @@
 var lastSong = null;
 var selection = null;
-var playlist = ["10.mp3", "audio/9.wav", "audio/7.wav", "audio/8.wav", "audio/6.wav", "audio/5.wav", "audio/4.wav", "audio/3.wav", "audio/2.wav",]; // List of Songs
+var playlist = ["audio/10.wav", "audio/11.mp3", "audio/9.wav", "audio/7.wav", "audio/8.wav", "audio/6.wav", "audio/5.wav", "audio/4.wav", "audio/3.wav", "audio/2.wav",]; // List of Songs
 var player = document.getElementById("audioplayer"); // Get Audio Element
 player.autoplay=true;
 player.addEventListener("ended", selectRandom); // Run function when song ends
