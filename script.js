2b06c1215a54c5f5ae7462d7006546d7a0b01c77
luke-github <57570751+luke-github@users.noreply.github.com>
2020-05-11 07:38:37 +0100

    Add seamless page transition features, no new files are now required.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-    
+
 })
+
+function toPage(frompage, topage) {
+    $(".menulisting#" + frompage).css("display", "none");
+    $(".menulisting#" + topage).css("display", "flex");
+}
