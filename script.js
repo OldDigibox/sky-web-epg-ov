cabf4e778f791a96a403e7d6cdc0b856874fd968
luke-github <57570751+luke-github@users.noreply.github.com>
2020-05-11 08:53:19 +0100

    Add 'Custom Pages' page and a 'light switch' toggle inside

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-$(document).ready(function() {
-
-})
+var isLightOn = true;
 
 function toPage(frompage, topage) {
     $(".menulisting#" + frompage).css("display", "none");
     $(".menulisting#" + topage).css("display", "flex");
+}
+
+function lightsOff() {
+    if (isLightOn) {
+        isLightOn = false;
+        $("body").get(0).style.setProperty("--background-image", "url('https://media.discordapp.net/attachments/621022891374542858/709301926642188308/darken.png')");
+        $("body").get(0).style.setProperty("--main-colour", "#222");
+        $("body").get(0).style.setProperty("--sub-colour", "#444");
+        $("body").get(0).style.setProperty("--listnum-colour", "white");
+    } else {
+        isLightOn = true;
+        $("body").get(0).style.setProperty("--background-image", "url('https://media.discordapp.net/attachments/649968233553723405/709086018942402631/image1.jpg')");
+        $("body").get(0).style.setProperty("--main-colour", "rgb(0, 0, 255)");
+        $("body").get(0).style.setProperty("--sub-colour", "rgb(0, 0, 179)");
+        $("body").get(0).style.setProperty("--listnum-colour", "rgb(126, 126, 255)");
+    }
 }
