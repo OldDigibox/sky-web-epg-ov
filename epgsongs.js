cac1c5754bb747ca9fc11e69d7a79a1d02f1352c
barney-heatwave <djdise@outlook.com>
2020-05-11 17:50:01 +0100

    Fix jumbled links

--- epgsongs.js
+++ epgsongs.js
@@ -1,6 +1,10 @@
+// TIME OF DAY
+var early_morning, morning, daytime, nightclub;
+
+// AUDIO PLAYER
 var lastSong = null;
 var selection = null;
-var playlist = ["audio/daytime/1998_trk04.mp3", "audio/daytime/2009_trk7.mp3", "audio/daytime/4mp3", "audio/daytime/connected.mp3", "audio/daytime/funkerama.mp3", "audio/daytime/gritty_groove.mp3", "audio/daytime/internationale!(remix)", "audio/daytime/no_saints.mp3", "audio/daytime/smooth_strings_trip.mp3", "audio/daytime/sunset.mp3", "audio/daytime/swingin_partay.mp3"]; // List of Songs
+var playlist = ["audio/daytime/1998_trk04.mp3", "audio/daytime/2009_trk7.mp3", "audio/daytime/4mp3", "audio/daytime/connected.mp3", "audio/daytime/funkerama.mp3", "audio/daytime/gritty_groove.mp3", "audio/daytime/internationale!(remix)", "audio/daytime/no_saints.mp3", "audio/daytime/smooth_strings_trip.mp3", "audio/daytime/sunset.mp3", "audio/daytime/swingin_partay.mp3"]; // Daytime set
 var player = document.getElementById("EPG_Background_Audio"); // Get Audio Element
 player.autoplay = true;
 player.addEventListener("ended", selectRandom); // Run function when song ends
