9591aa37db6ea867522ecbf99618a57d92e07707
luke-github <57570751+luke-github@users.noreply.github.com>
2020-05-11 02:06:12 +0100

    Make margins between list items smaller

--- epgsongs.js
+++ epgsongs.js
@@ -1,16 +1,17 @@
 var lastSong = null;
 var selection = null;
-var playlist = ["audio/12.mp3", "audio/13mp3", "audio/14mp3", "audio/15mp3", "audio/10.wav", "audio/11.mp3", "audio/9.wav", "audio/7.wav", "audio/8.wav", "audio/6.wav", "audio/5.wav", "audio/4.wav", "audio/3.wav", "audio/2.wav",]; // List of Songs
+var playlist = ["audio/2.mp3", "audio/3.mp3", "audio/4.mp3", "audio/5.mp3", "audio/6.mp3", "audio/7.mp3", "audio/8.mp3", "audio/9.mp3", "audio/10.mp3", "audio/11.mp3", "audio/12.mp3", "audio/13.mp3", "audio/14.mp3", "audio/15.mp3"]; // List of Songs
 var player = document.getElementById("audioplayer"); // Get Audio Element
-player.autoplay=true;
+player.autoplay = true;
 player.addEventListener("ended", selectRandom); // Run function when song ends
 
-function selectRandom(){
-    while(selection == lastSong){ // Repeat until different song is selected
+function selectRandom() {
+    while (selection == lastSong) { // Repeat until different song is selected
         selection = Math.floor(Math.random() * playlist.length);
     }
     lastSong = selection; // Remember last song
     player.src = playlist[selection]; // Tell HTML the location of the new Song
+
 }
 
 selectRandom(); // Select initial song
