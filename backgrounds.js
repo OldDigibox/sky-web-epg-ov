be27b0b3e5755256ba892f5eec2c30096a2645d7
barney-heatwave <djdise@outlook.com>
2020-05-13 23:43:33 +0100

    Make gap between buttons seem more like the original

--- backgrounds.js
+++ backgrounds.js
@@ -1,6 +1,6 @@
 function toPage(frompage, topage) {
     $(".menulisting#" + frompage).css("display", "none");
     $(".menulisting#" + topage).css("display", "flex");
-    $('main').toggleClass('bground2');
-    $('adult').toggleClass('bground2');
+    $('main').toggleClass('bground2')
+    $('custom').toggleClass('bground2')
 }
