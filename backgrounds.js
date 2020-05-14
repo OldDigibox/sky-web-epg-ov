a73df199d30adcb4ba5941bea94f8eaf59a6dbfa
barney-heatwave <djdise@outlook.com>
2020-05-14 18:39:25 +0100

    MORE page does not use blank background

--- backgrounds.js
+++ backgrounds.js
@@ -1,6 +1,5 @@
 function toPage(frompage, topage) {
     $(".menulisting#" + frompage).css("display", "none");
     $(".menulisting#" + topage).css("display", "flex");
-    $('main').toggleClass('bground2')
-    $('custom').toggleClass('bground2')
+    $('adult').toggleClass('bground2')
 }
